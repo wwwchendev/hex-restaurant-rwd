138f835f66f35eb1e9836ddd6c3fc99c38bd2ec9
wwwchendev <wwwchendev@gmail.com>
2024-01-19 00:46:57 +0800

    feat✨: 完成首頁樣式

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -4,6 +4,7 @@ const navEl = document.querySelector('header ul');
 // 點擊 button 切換 active 類別
 toggleNavBtn.addEventListener('click', () => {
   navEl.classList.toggle('active');
+  document.querySelector('header').classList.add('active');
 });
 
 // 取得所有的 li 元素
@@ -13,5 +14,14 @@ navItems.forEach((item) => {
     if (navEl.classList.contains('active')) {
       navEl.classList.remove('active');
     }
+    document.querySelector('header').classList.remove('active');
   });
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 150) {
+    document.querySelector('header').classList.add('active');
+  } else {
+    document.querySelector('header').classList.remove('active');
+  }
 });
